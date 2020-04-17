cd43e2d3f25e4a086124829fe47ecd39fecf35b9
Marissa <marissacooterl@gmail.com>
2020-04-17 11:16:22 -0500

    built out components, moving on to hooking them up to app

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -2,3 +2,23 @@ import React, {useContext} from 'react';
 import {SmurfsContext} from '../context/SmurfsContext';
 import Create from './Create';
 
+const SmurfList = () => {
+    const smurfs = useContext(SmurfsContext);
+    return (
+        <div>
+            {console.log(smurfs)}
+            <Create />
+            {smurfs.map((smurf, index) => {
+                return (
+                    <div key={index}>
+                        <p>name:{smurf.name}</p>
+                        <p>age:{smurf.age}</p>
+                        <p>height:{smurf.height}</p>
+                    </div>
+                )
+            })}
+        </div>
+    );
+};
+
+export default SmurfsList
